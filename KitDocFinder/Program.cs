1865079bef0769ea5ed58a2bb81ba6c1d71b8c8c
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-05-10 14:41:52 +0300

    Добавьте файлы проекта.

---
+++ KitDocFinder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KitDocFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KitDocFinder начало работы...");
+        }
+    }
+}
