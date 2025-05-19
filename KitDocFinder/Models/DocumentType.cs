117ab73742cc967d755cba704ca0b734bd40e9b8
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-05-19 23:43:52 +0300

    Добавление основных моделей данных

---
+++ KitDocFinder/Models/DocumentType.cs
@@ -0,0 +1,17 @@
+﻿namespace KitDocFinder.Models
+{
+    /// <summary>
+    /// Модель данных для сущности "Типа документа"
+    /// </summary>
+    public class DocumentType
+    {
+        /// <summary>
+        /// Идентификатор сущности "Тип документа"
+        /// </summary>
+        public int Id {  get; set; }
+        /// <summary>
+        /// Название типа документа
+        /// </summary>
+        public required string Title { get; set; }
+    }
+}
