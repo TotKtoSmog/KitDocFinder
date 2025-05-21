bb1ab1c2315513a48c14e13f6d245b04f65369be
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-05-21 16:37:08 +0300

    Подключение контекста данных

--- KitDocFinder/Program.cs
+++ KitDocFinder/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace KitDocFinder
+﻿using KitDocFinder.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace KitDocFinder
 {
     /// <summary>
     /// Точка входа в приложение
@@ -8,6 +14,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("KitDocFinder начало работы...");
+            var host = Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(context.Configuration.GetConnectionString("DefaultConnection")));
+            })
+            .Build();
         }
     }
 }
